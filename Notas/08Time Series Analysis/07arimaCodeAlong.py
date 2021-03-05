93140f760280d2f43034a184bbd6c202a5e636cc
Pedro Pla <ppla2001@gmail.com>
2021-03-04 21:01:48 -0300

    termine arima code along

--- Notas/08Time Series Analysis/07arimaCodeAlong.py
+++ Notas/08Time Series Analysis/07arimaCodeAlong.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from statsmodels.tsa.holtwinters import model
 # %%
 df = pd.read_csv('C:\\Users\\plape\\OneDrive\\Escritorio\\Python-Finance\\Python-Finance\\Notebooks\\08-Time-Series-Analysis\\monthly-milk-production-pounds-p.csv')
 # %%
@@ -85,4 +86,64 @@ df['Seasonal First Difference'] = df['First Difference'] - df['First Difference'
 df['Seasonal First Difference'].plot()
 # %%
 adf_check(df['Seasonal First Difference'].dropna())
+# %%
+#ACF & PACF
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
+# %%
+fig_first = plot_acf(df['First Difference'].dropna())
+# %%
+fig_seasonal_first = plot_acf(df['Seasonal First Difference'].dropna())
+# %%
+from pandas.plotting import autocorrelation_plot
+# %%
+autocorrelation_plot(df['Seasonal First Difference'].dropna())
+# %%
+#Partial Correlation Plot
+result = plot_pacf(df['Seasonal First Difference'].dropna())
+# %%
+#FINAL ACF PACF plots 
+plot_acf(df['Seasonal First Difference'].dropna())
+plot_pacf(df['Seasonal First Difference'].dropna())
+# %%
+#Como la data es seasonal, vamos a usar seasonal arima model
+from statsmodels.tsa.arima_model import ARIMA
+# %%
+model = sm.tsa.statespace.SARIMAX(df['Milk in Pounds per Cow'],order=(0,1,0),seasonal_order=(1,1,1,12)) #order, seasonal order
+# %%
+results = model.fit()
+# %%
+print(results.summary())
+# %%
+#residual errors
+results.resid
+# %%
+results.resid.plot()
+# %%
+results.resid.plot(kind='kde')
+# %%
+#No puedo hacer forecast mas alla de la cantidad de puntos de data q hay en el df, para eso le tengo q agregar tiempo al df usando pandas
+df['forecast'] = results.predict(start=150,end=168)
+df[['Milk in Pounds per Cow','forecast']].plot(figsize=(12,8))
+# %%
+#agrego tiempo
+from pandas.tseries.offsets import DateOffset
+# %%
+future_dates = [df.index[-1] + DateOffset(months=x) for x in range(1,24)]
+# %%
+future_dates
+# %%
+future_df = pd.DataFrame(index=future_dates,columns=df.columns)
+# %%
+future_df
+# %%
+final_df = pd.concat([df,future_df])
+# %%
+final_df['forecast'] = results.predict(start=168,end=192)
+# %%
+final_df.tail()
+# %%
+final_df['Milk in Pounds per Cow'].plot()
+final_df['forecast'].plot()
+# %%
+final_df[['Milk in Pounds per Cow','forecast']].plot(figsize=(12,8))
 # %%
